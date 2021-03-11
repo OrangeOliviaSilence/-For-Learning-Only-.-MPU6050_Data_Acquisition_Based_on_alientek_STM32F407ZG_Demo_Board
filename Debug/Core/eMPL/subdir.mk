aae7d93963fc173bac473a57e492fb69329c335c
嘉厚纯粹_Bourne <137153189@qq.com>
2021-03-11 16:13:31 +0800

    Add files via upload

---
+++ Debug/Core/eMPL/subdir.mk
@@ -0,0 +1,25 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (9-2020-q2-update)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Core/eMPL/inv_mpu.c \
+../Core/eMPL/inv_mpu_dmp_motion_driver.c 
+
+OBJS += \
+./Core/eMPL/inv_mpu.o \
+./Core/eMPL/inv_mpu_dmp_motion_driver.o 
+
+C_DEPS += \
+./Core/eMPL/inv_mpu.d \
+./Core/eMPL/inv_mpu_dmp_motion_driver.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Core/eMPL/inv_mpu.o: ../Core/eMPL/inv_mpu.c Core/eMPL/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DUSE_HAL_DRIVER -DDEBUG -DSTM32F407xx -c -I../Core/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../Drivers/CMSIS/Include -I"E:/STM32CubeIDE_workspace_1.5.1/TemplateMPU6050/Core/eMPL" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Core/eMPL/inv_mpu.d" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+Core/eMPL/inv_mpu_dmp_motion_driver.o: ../Core/eMPL/inv_mpu_dmp_motion_driver.c Core/eMPL/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DUSE_HAL_DRIVER -DDEBUG -DSTM32F407xx -c -I../Core/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../Drivers/CMSIS/Include -I"E:/STM32CubeIDE_workspace_1.5.1/TemplateMPU6050/Core/eMPL" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Core/eMPL/inv_mpu_dmp_motion_driver.d" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+
